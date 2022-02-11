d98ca4377969fefe23b3d92381d2858ee172c6d5
Colby Kauk <colby.kauk@gmail.com>
2022-02-10 16:44:49 -0800

    update diakatchiro to diakachi

---
+++ java_spring/daikachiroutes/src/main/java/com/colby/diakachiroutes/DaikachiroutesApplication.java
@@ -0,0 +1,13 @@
+package com.colby.diakachiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikachiroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikachiroutesApplication.class, args);
+	}
+
+}
